9132215cf2ebf064824187a68d8c3523536346e9
mtc2013 <mtc2013opensource@gmail.com>
2016-08-19 08:35:34 -0400

    Add additional day by day query

--- lib/project_metric_slack_trends.rb
+++ lib/project_metric_slack_trends.rb
@@ -28,10 +28,15 @@ class ProjectMetricSlackTrends
   def get_week_of_slack_data week_number, member_names
     start_time = (Time.now - (7*week_number+Time.now.wday+1).days).to_s[0,10]
     end_time = (Time.now - (7*(week_number-1)+Time.now.wday).days).to_s[0,10]
-    member_names.inject({}) do |slack_message_totals, user_name|
+    slack_message_totals = member_names.inject({}) do |slack_message_totals, user_name|
       num_messages = @client.search_all(query: "from:#{user_name} after:#{start_time} before:#{end_time}").messages.matches.select { |m| m.channel.name == @channel }.length
       slack_message_totals.merge user_name => num_messages
     end
+    (0..6).each do |day_of_week|
+      day = (Time.now - (7*week_number+Time.now.wday+day_of_week).days).to_s[0,10]
+      slack_message_totals[day_of_week.to_s] = @client.search_all(query: "on:#{day}").messages.matches.select { |m| m.channel.name == @channel }.length
+    end
+    slack_message_totals
   end
 
   def get_member_names_for_channel
