a741cd59b7ced3b11dc14f166d88dabd30c1bfe6
mtc2013 <mtc2013opensource@gmail.com>
2016-09-06 17:37:01 -0400

    First steps towards better query usage

--- spec/project_metric_slack_trends_spec.rb
+++ spec/project_metric_slack_trends_spec.rb
@@ -1,4 +1,5 @@
 require 'project_metric_slack_trends'
+require 'byebug'
 
 describe ProjectMetricSlackTrends, :vcr do
 
@@ -10,9 +11,12 @@ describe ProjectMetricSlackTrends, :vcr do
   describe '#refresh' do
 
     context 'meaningful raw_data' do
-      let(:raw_data) { {"week_one" => {"an_ju" => 0, "armandofox" => 0, "francis" => 0, "intfrr" => 0, "mtc2013" => 19, "tansaku" => 0, "0" => 0, "1" => 0, "2" => 0, "3" => 0, "4" => 1, "5" => 0, "6" => 0},
-                        "week_three" => {"an_ju" => 0, "armandofox" => 10, "francis" => 0, "intfrr" => 0, "mtc2013" => 0, "tansaku" => 0, "0" => 0, "1" => 0, "2" => 0, "3" => 8, "4" => 0, "5" => 0, "6" => 0},
-                        "week_two" => {"an_ju" => 0, "armandofox" => 5, "francis" => 0, "intfrr" => 0, "mtc2013" => 2, "tansaku" => 10, "0" => 1, "1" => 0, "2" => 0, "3" => 0, "4" => 5, "5" => 1, "6" => 0}} }
+      let(:raw_data) { {"week_one"=>{"mtc2013"=>0, "tansaku"=>0, "jinksy"=>0, "marouen"=>1, "ksteph"=>1, "an_ju"=>1,
+                                     "armandofox"=>1, "0"=>0, "1"=>0, "2"=>0, "3"=>2, "4"=>0, "5"=>33, "6"=>23},
+                        "week_two"=>{"mtc2013"=>1, "tansaku"=>1, "jinksy"=>0, "marouen"=>0, "ksteph"=>0, "an_ju"=>0,
+                                     "armandofox"=>0, "0"=>0, "1"=>0, "2"=>0, "3"=>0, "4"=>0, "5"=>0, "6"=>0},
+                        "week_three"=>{"mtc2013"=>0, "tansaku"=>0, "jinksy"=>0, "marouen"=>0, "ksteph"=>0, "an_ju"=>0,
+                                       "armandofox"=>0, "0"=>0, "1"=>0, "2"=>0, "3"=>0, "4"=>0, "5"=>0, "6"=>0}} }
       it 'fetches raw data' do
         subject.refresh
         expect(subject.raw_data).to eq(raw_data)
@@ -51,7 +55,7 @@ describe ProjectMetricSlackTrends, :vcr do
 
   describe '#score' do
     it 'computes a score' do
-      expect(ProjectMetricSlackTrends.new(channel: 'projectscope', token: ENV["SLACK_API_TOKEN"]).score).to eq 0.0713333333333333
+      expect(ProjectMetricSlackTrends.new(channel: 'projectscope', token: ENV["SLACK_API_TOKEN"]).score).to eq 0.2651428571428572
     end
   end
 
